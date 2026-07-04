b569aa2ecbd642ae91b31417934d93c5cd4d944e
Amit-exe <amit.kushwaha.exe@gmail.com>
2026-07-04 20:24:48 +0530

    added winston logger, reading summary option, fixed favicon and title

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -1,10 +1,16 @@
 const AppError = require("./AppError");
+const logger = require("../utils/logger");
 
 function errorHandler(err, req, res, next) {
   const code = err.status || 500;
   const message = err.message || "something went wrong!";
 
-  if (!(err instanceof AppError)) console.error(err);
+  if (!(err instanceof AppError))
+    logger.error({
+      message: err.message,
+      stack: err.stack,
+      status: err.status,
+    });
 
   return res.status(code).json({ code, message });
 }
